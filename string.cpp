a18f6490dc190c338c39c39604d1487a4ce165bb
Mamoonkhan2 <hussainali197676@gmail.com>
2025-08-19 00:02:47 +0500

    string

--- string.cpp
+++ string.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 
 int main() {
-
-
+    char string[100] = "Hello, World!";
+    char arr[] = {
+        'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\0'
+    };
+    for(int i = 0; arr[i] != '\0'; i++) {
+        cout<<arr[i];
+    }
     return 0;
 }
