794a05d1ed9787d20a5cf160729c80302d9a5c11
Mamoonkhan2 <hussainali197676@gmail.com>
2025-08-29 21:47:57 +0500

    string

--- string.cpp
+++ string.cpp
@@ -2,12 +2,61 @@
 #include <vector>
 #include <cstring>
 #include <string>
+#include <cctype>
 using namespace std;
 
+bool hhhh(char c){
+    if((c>=97)&&(c<=122))
+    {
+        return true;
+    }
+    return false;
+}
 
+bool hhhh(int c){
+    if((c>=0)&&(c<=9))
+    {
+        return true;
+    }
+    return false;
+}
 int main() {
-    string name;
-    getline(cin, name, '.');
-    cout << name;
+    string names = "$^%r&a##c&##!c&a#r*&(#)";
+    string part  = "##";
+    while (names.length()>0&&names.find(part)<names.length())
+    {
+        int j = names.find(part);
+        names.erase(j,part.length());
+    }
+    cout<<names;
+    // int i = 0;
+    // int j = names.length()-1;
+    // bool yes = false;
+    // while (i<j)
+    // {
+    //     if(!hhhh(names[i])){
+    //         i++;
+    //         continue;
+    //     }
+    //     else if(!hhhh(names[j])){
+    //         j--;
+    //         continue;
+    //     }
+    //     else if(names[i]==names[j]&&hhhh(names[i])&&hhhh(names[j]))
+    //     {
+    //         yes = true;
+    //         i++;
+    //         j--;
+    //     }
+    //     else{
+    //         yes = false;
+    //     }
+    // }
+    // if(yes){
+    //     cout<<"palendrom";
+    // }
+    // else{
+    //     cout<<"not palendrom";
+    // }
     return 0;
 }
