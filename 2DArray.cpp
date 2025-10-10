d5a24b4103c73f976a0c7ec2123822c382371873
Mamoonkhan2 <hussainali197676@gmail.com>
2025-10-11 00:11:37 +0500

    binary search in 2d matrix

--- 2DArray.cpp
+++ 2DArray.cpp
@@ -1,106 +1,47 @@
 #include <iostream>
 #include <vector>
-<<<<<<< HEAD
-#include <array>
-
-using namespace std;
-pair<int, int> linear(int matrix[][6], int key)
-{
-    pair<int, int> h;
-    for (int row = 0; row < 4; row++)
-    {
-        for (int col = 0; col < 3; col++)
-        {
-            if (matrix[row][col] == 5)
-            {
-            }
-        }
-    }
-    return h = {1, 1};
-}
-void sum(int matrix[][3])
-{
-    int maxe = -666;
-    for (int row = 0; row < 3; row++)
-    {
-
-        int h = 0;
-        for (int col = 0; col < 3; col++)
-        {
-            h += matrix[col][row];
-            maxe = max(maxe, h);
-        }
-        cout << maxe << endl;
-    }
-}
-int main()
-{
-    // int matrix[4][4] = {
-    //     {1,  2,  3,  4  },
-    //     {5,  6,  7,  8  },
-    //     {9,  10, 11, 12 },
-    //     {13, 14, 15, 16 }
-    // };
-    // for (int row = 0; row < 3; row++)
-    // {
-    //     for (int col = 0; col < 3; col++)
-    //     {
-    //         cout << "enter the value ";
-    //         cin >> matrix[row][col];
-    //     }
-    // }
-    // int row = 2;
-    // while(col>=0){
-    //     cout<<matrix[row][col];
-    //     row+=1;
-    //     col+=1;
-    // }
-    // 00 02
-    // 11 11
-    // 22 20
-    vector <vector<int>> matrix[3][3] ={ {1,2,3}
-                                        ,{4,5,6},
-                                         {7,8,9}};
-    int sums=0;
-    //     int matrix[4][4] = {
-        // {1,  2,  3,  4  },
-    //     {5,  6,  7,  8  },
-    //     {9,  10, 11, 12 },
-    //     {13, 14, 15, 16 }
-    // };
-    // for (int i = 0; i < 4; i++) {
-    //     for (int j = 0; j < 4; j++) {
-    //         if(i==j){
-    //             sums+=matrix[i][j];
-    //         }
-    //         else if(j==4-i-1){
-    //             sums+=matrix[i][j];
-    //         }
-    //     }
-    // }
-    for (int i = 0; i < 3; i++) {
-        sums += matrix[i][i];
-
-        
-    cout<<sums;
-=======
 using namespace std;
 
 
 int main() {
-    int  matrix[4][3] = {
-
-                {1   , 2  ,  3}, 
-                {4   , 5  ,  6},   
-                {7   , 8  ,  9}
-                                };
-    for (int row = 0; row < 3; row++) {
-        for (int col = 0; col < 3; col++) {
-            cout<<matrix[row][col]<<" ";
+    vector <vector<int>> matrix = {
+        {1,3,6,9},
+        {12,15,18,21},
+        {24,27,30,35},
+    };
+    int target = 27;
+    int i = 0;
+    int end = matrix.size();
+    while (i>=0)
+    {
+        int mid = (i+end)/2;
+        if(target>=matrix[mid][0]&&target<=matrix[mid][matrix.size()-1]){
+            i = mid;
+            break;
         }
-        cout<<endl;
+        else if(target>matrix[i][matrix.size()-1]){
+            i = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
     }
-
->>>>>>> a7828e37428c1278ab6dbb6cff5ad61b473ed25d
+    int start = 0;
+    int hoo   =     matrix[i].size();
+    while(start<3){
+        int mid = (start+hoo)/2;
+        if(matrix[i][mid]==target){
+            cout<<matrix[i][mid]<<endl;
+            cout<<i<<" "<<mid;
+            
+            break;
+        }
+        else if(target>matrix[i][mid]){
+            start = mid + 1;
+        }
+        else{
+            hoo = mid - 1;
+        }
+    }
     return 0;
 }
